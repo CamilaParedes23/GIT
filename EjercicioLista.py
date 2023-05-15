41b1d157628807dff01d79cd5b9b12335c716f92
Camila Paredes <csparedes2@espe.edu.ec>
2023-05-15 10:33:38 -0500

    Ejercicio Lista

---
+++ EjercicioLista.py
@@ -0,0 +1,6 @@
+#UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+#Nombre: Paredes Panata Camila Soledad
+#NRC: 9898
+subjects = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+for subject in subjects:
+    print("Yo estudio " + subject)
---
+++ README.md
@@ -0,0 +1,9 @@
+# Estructura de Datos
+
+En este repositorio de código se dubhirá la información de los ejemplos de las distintas clases que vayamos revisando.
+
++ TDA
++ Memoria Estática
++ Memoria Dinámica
++ Recursividad
++ Listas
